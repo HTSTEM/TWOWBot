66eb586fc496160db7d54ed26b098acaf52d8f97
Bottersnike <bottersnike237@gmail.com>
2017-07-01 19:02:09 +0100

    Add 'twow' cog

---
+++ cogs/twow.py
@@ -0,0 +1,28 @@
+import subprocess
+import asyncio
+import inspect
+import sys
+
+from discord.ext import commands
+import discord
+
+from cogs.util import checks
+
+
+class TWOW:
+    '''TWOW commands'''
+    
+    @commands.command()
+    async def status(self, ctx):
+        '''Get's current TWOW status'''
+        
+        embed = discord.Embed(colour=0xFFFF00)
+        embed.add_field(name='Servers', value="0")
+        embed.add_field(name='Channels', value="0")
+        embed.add_field(name='Games All Time', value="0")
+        embed.add_field(name='Games In Progress', value="0")
+        embed.set_footer(text='Footer')
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(TWOW())
