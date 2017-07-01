5bdc9523418a7911650251f567665e90d60faffd
Hans Shi <hanss314@gmail.com>
2017-07-01 11:23:22 -0700

    Added setup command

--- cogs/twow.py
+++ cogs/twow.py
@@ -23,6 +23,16 @@ class TWOW:
         embed.add_field(name='Games In Progress', value="0")
         embed.set_footer(text='Footer')
         await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def setup(self, ctx):
+        channel_id = ctx.channel.id
+        if False:
+            #TODO: check if channel in database
+            await ctx.send('This channel is already setup!')
+        else:
+            #TODO: create database
+            await ctx.send('This channel is ready to play a minitwow!')        
 
 def setup(bot):
     bot.add_cog(TWOW())
