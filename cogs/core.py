f2a5821147a23d481d1de8cb5d69f0b6e64aea18
hanss314 <hanss314@gmail.com>
2017-08-04 14:00:24 -0700

    Added core functions

--- cogs/core.py
+++ cogs/core.py
@@ -1,3 +1,5 @@
+import datetime
+
 from discord.ext import commands
 import ruamel.yaml as yaml
 import discord
@@ -42,6 +44,52 @@ class Core():
             
         d += '\n*Made by Bottersnike#3605, hanss314#0128 and Noahkiq#0493*'
         await ctx.bot.send_message(ctx.channel, d)
+        
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.bot.send_message(ctx.channel, 'Pong!')
+        
+    @commands.command(aliases=['info'])
+    async def about(self, ctx):
+        mess = '**This bot was developed by:**\n'
+        mess += 'Bottersnike#3605\n'
+        mess += 'hanss314#0128\n'
+        mess += 'Noahkiq#0493\n'
+        mess += '\n**This bot is being hosted by:**\n'
+        mess += '{}\n'.format(ctx.bot.get_user(ctx.bot.BOT_HOSTER))
+        #mess +='**\nTWOWBot\'s avatar by:**\n'
+        #mess += 'name#discrim\n'
+        mess += '\n**Resources:**\n'
+        mess += '*The official TWOWBot discord server:* https://discord.gg/eZhpeMM\n'
+        mess += '*Go contribute to TWOWBot on GitHub:* https://github.com/HTSTEM/TWOW_Bot\n'
+        mess += '*Invite TWOWBot to your server:* <https://discordapp.com/oauth2/authorize?client_id={}&scope=bot>'.format(ctx.bot.user.id)
+        await ctx.bot.send_message(ctx.channel, mess)
+        
+    @commands.command(aliases=['aboutme','boutme','\'boutme'])
+    async def me(self, ctx):
+        member = ctx.author
+        now = datetime.datetime.utcnow()
+        joined_days = now - member.joined_at
+        created_days = now - member.created_at
+        avatar = member.avatar_url
+
+        embed = discord.Embed(colour=member.colour)
+        embed.add_field(name='Nickname', value=member.display_name)
+        embed.add_field(name='User ID', value=member.id)
+        embed.add_field(name='Avatar', value='[Click here to show]({})'.format(avatar))
+
+        embed.add_field(name='Created', value=member.created_at.strftime('%x %X') + '\n{} days ago'.format(max(0, created_days.days)))
+        embed.add_field(name='Joined', value=member.joined_at.strftime('%x %X') + '\n{} days ago'.format(max(0, joined_days.days)))
+        roles = '\n'.join([r.mention for r in sorted(member.roles, key=lambda x:x.position, reverse=True) if r.name != '@everyone'])
+        if roles == '': roles = '\@everyone'
+        embed.add_field(name='Roles', value=roles)
+
+        embed.set_author(name=member, icon_url=avatar)
+
+        try:
+            await ctx.channel.send(embed=embed)
+        except discord.errors.Forbidden:
+            pass
 
 def setup(bot):
     bot.add_cog(Core())
